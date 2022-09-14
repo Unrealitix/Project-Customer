eb0edd8483bb91eb05d52bf589df11e9d5408f42
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2022-09-14 23:56:33 +0200

    VR implementation! + Game is now OpenXR + No more PropDescManager.cs needed; all props spawn their own panel from a prefab that is loaded by the script (no need for dragging anything in) + Removed temp scripts

--- Assets/Scripts/Prop.cs
+++ Assets/Scripts/Prop.cs
@@ -1,26 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Prop : MonoBehaviour
 {
 	[SerializeField] private string propName;
 	[TextArea(3, 10)] [SerializeField] private string description;
+	private GameObject _descriptionPanel;
+	private GameObject _descriptionPanelPrefab;
+	private XRGrabInteractable _grabInteractable;
 
-	private PropDescManager _propDescManager;
+	private Rigidbody _rigidbody;
+
+	private Vector3 _startPosition;
 
 	private void Start()
 	{
-		_propDescManager = FindObjectOfType<PropDescManager>();
+		_startPosition = transform.position;
+		_rigidbody = GetComponent<Rigidbody>();
+
+		_grabInteractable = GetComponent<XRGrabInteractable>();
+		if (_grabInteractable == null)
+		{
+			Debug.LogError($"Prop {propName} does not have an XRGrabInteractable component attached to it.");
+		}
+		else
+		{
+			_grabInteractable.selectEntered.AddListener(Grab);
+			_grabInteractable.selectExited.AddListener(Release);
+			_grabInteractable.movementType = XRBaseInteractable.MovementType.VelocityTracking;
+		}
+
+		_descriptionPanelPrefab = Resources.Load<GameObject>("Prop Description Panel");
 	}
 
-	public void Grab()
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log("Exited collision " + other.gameObject.name);
+		if (other.gameObject.CompareTag("Resetter"))
+		{
+			_rigidbody.position = _startPosition;
+			_rigidbody.velocity = Vector3.zero;
+			_rigidbody.rotation = Quaternion.identity;
+			_rigidbody.angularVelocity = Vector3.zero;
+		}
+	}
+
+	private void Grab(SelectEnterEventArgs selectEnterEventArgs)
 	{
 		// Debug.Log("Grabbed");
-		_propDescManager.GrabProp(this, propName, description);
+		SpawnPanel(selectEnterEventArgs.interactorObject.transform.gameObject.GetNamedChild("Panel Origin").transform);
 	}
 
-	public void Release()
+	private void SpawnPanel(Transform parent)
+	{
+		if (_descriptionPanel != null) return;
+
+		_descriptionPanel = Instantiate(_descriptionPanelPrefab, parent);
+		_descriptionPanel.GetComponent<Canvas>().worldCamera = Camera.main;
+
+		//put in the right strings into the text objects
+		List<TextMeshProUGUI> texts = _descriptionPanel.GetComponentsInChildren<TextMeshProUGUI>().ToList();
+		texts.ForEach(text =>
+		{
+			text.text = text.text switch
+			{
+				{ } a when a.IndexOf("name", StringComparison.OrdinalIgnoreCase) >= 0 => propName,
+				{ } a when a.IndexOf("description", StringComparison.OrdinalIgnoreCase) >= 0 => description,
+				_ => text.text,
+			};
+		});
+	}
+
+	private void Release(SelectExitEventArgs selectExitEventArgs)
 	{
 		// Debug.Log("Released");
-		_propDescManager.DisablePropDesc();
+		Destroy(_descriptionPanel);
 	}
 }
